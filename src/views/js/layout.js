f6fb88a57c1d1590e4dbc2550744a7024400a502
shubhajit-paul-web <shubhajitpaul717@gmail.com>
2026-01-22 01:44:50 +0530

    Enhance sidebar functionality and add My Files page

--- src/views/js/layout.js
+++ src/views/js/layout.js
@@ -2,5 +2,9 @@ const toggleSidebarBtn = document.querySelector("#toggle-sidebar-btn");
 const sidebar = document.querySelector("#sidebar");
 
 toggleSidebarBtn.addEventListener("click", () => {
-	sidebar.classList.toggle("hidden");
+	if (sidebar.style.left === "0px") {
+		sidebar.style.left = "-26vw";
+	} else {
+		sidebar.style.left = "0px";
+	}
 });
